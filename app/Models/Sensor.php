bce0554a05f5b13a142cc4bd16dab1f017f8a2a3
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-08-12 15:45:22 -0300

    first commit

---
+++ app/Models/Sensor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    protected $fillable = [
+    'ambiente_id',
+    'codigo',
+    'tipo',
+    'descricao',
+    'status'
+    ];
+    
+    public function registros(){
+        return $this->hasMany(Registro::class);
+    }
+
+    public function ambiente(){
+        return $this->belongsTo(Ambiente::class);
+    }
+}
