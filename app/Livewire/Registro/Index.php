8265628aaca854072be9799af5374c1f402263c1
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-09-16 15:35:12 -0300

    listagem

--- app/Livewire/Registro/Index.php
+++ app/Livewire/Registro/Index.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire\Registro;
 
+use App\Models\Registro;
+use App\Models\Sensor;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
+  
+
+    public $search = '';
+    public $perPage = 10;
+    
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage' => ['except' => 10],
+    ];
+
+    public function render()
+    {
+        $ambiente = Ambiente::all();
+        return view('registro.index', compact('registro'));
+    }
+public function delete($id){
+    $registro_id =  Registro::find($id);
+
+    $registro_id->delete();
+}
+
+}
         return view('livewire.registro.index');
     }
-}
