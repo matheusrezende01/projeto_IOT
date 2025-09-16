7a8fbc199084815c26d2c0d0a6d5626adda198e9
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-09-16 15:35:05 -0300

    listagem de registro

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\RegistroController;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('registro/create', [RegistroController::class, "store"]);
+
+Route::get('registro/index', [RegistroController::class, "index"]);
