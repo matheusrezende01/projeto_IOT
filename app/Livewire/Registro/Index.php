2b2d1f46e4d30ccbfde709fd89ca99ab5176008f
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-09-30 13:31:25 -0300

    alteração do index de registro

--- app/Livewire/Registro/Index.php
+++ app/Livewire/Registro/Index.php
@@ -5,32 +5,32 @@ namespace App\Livewire\Registro;
 use App\Models\Registro;
 use App\Models\Sensor;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
-    public function render()
-    {
-  
+  use WithPagination;
 
-    public $search = '';
-    public $perPage = 10;
-    
-    protected $queryString = [
-        'search' => ['except' => ''],
-        'perPage' => ['except' => 10],
-    ];
+  protected $paginationTheme = 'bootstrap';
 
-    public function render()
-    {
-        $ambiente = Ambiente::all();
-        return view('registro.index', compact('registro'));
-    }
-public function delete($id){
-    $registro_id =  Registro::find($id);
+  public $search = '';
+  public $perpage = 15;
 
-    $registro_id->delete();
+  protected $queryString = [
+  'search' => ['except' => ''],
+  'perpage' => ['except' => 15]
+  ];
+
+  public function render(){
+    $registros = Registro::where('sensor_id', 'like', "{this->search}%")
+    ->orWhere('valor', 'like', "{this->search}%")
+    ->orwhere('unidade', 'like', "{this->search}%")
+    ->orwhere('data_hora', 'like', "{this->search}%")
+    ->orderByDesc('id', 'data_hora', 'valor', 'unidade', 'sensor_id')
+    ->paginate($this->perpage);
+
+    return view('livewire.registro.index', compact('registros'));
+  }
+
+
 }
-
-}
-        return view('livewire.registro.index');
-    }
