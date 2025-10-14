d7aafed37382b159e949a1f9191ed1109dab51b8
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-10-14 15:42:25 -0300

    conectando o esp

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\SensorController;
+use App\Livewire\Sensor\SensorList;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('registro/create', [RegistroController::class, "store"]);
 
 Route::get('registro/index', [RegistroController::class, "index"]);
+
+Route::get('sensor/status', [SensorController::class, "status"]);
+
+Route::put('sensor/update', [SensorController::class, "update"]);
